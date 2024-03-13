7b2b2045b8a370490c8b49ccd5f2ed964dfc0a06
NhatHONGUYEN <hnnhat67@gmail.com>
2024-03-13 09:06:14 +0100

    💄 F03 : LoginPage > add layout

--- src/component/pages/login/LoginForm.jsx
+++ src/component/pages/login/LoginForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export default function LoginForm() {
   //state
@@ -19,7 +20,7 @@ export default function LoginForm() {
 
   //affichage (render)
   return (
-    <form action="submit" onSubmit={handleSubmit}>
+    <LoginFormStyled action="submit" onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
       <br />
       <h2>Connectez-vous</h2>
@@ -32,6 +33,10 @@ export default function LoginForm() {
       />
       <button>Accédez à mon espace</button>
       <Link to="/order"></Link>
-    </form>
+    </LoginFormStyled>
   );
 }
+
+const LoginFormStyled = styled.form`
+  background: pink;
+`;
---
+++ src/component/reusable-ui/Logo.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function Logo() {
+  return (
+    <LogoStyled>
+      <h1>CRAZEE</h1>
+      <h1>BURGER</h1>
+    </LogoStyled>
+  );
+}
+const LogoStyled = styled.div`
+  background: green;
+`;
