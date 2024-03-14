cff3cbb844e47f423535fbd9792a4964f42ac5d9
NhatHONGUYEN <hnnhat67@gmail.com>
2024-03-14 14:20:38 +0100

    ♻️ F03:LOGIN > LoginForm > Create <Input/>

--- src/component/pages/login/LoginForm.jsx
+++ src/component/pages/login/LoginForm.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { BsPersonCircle } from "react-icons/bs";
 import { IoChevronForward } from "react-icons/io5";
+import Input from "./Input";
 
 export default function LoginForm() {
   //state
@@ -23,23 +24,26 @@ export default function LoginForm() {
   //affichage (render)
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous !</h1>
-      <hr />
-      <h2>Connectez-vous</h2>
-      <div className="input-with-icon">
-        <BsPersonCircle className="icon" />
-        <input
+      <div>
+        <h1>Bienvenue chez nous !</h1>
+        <hr />
+        <h2>Connectez-vous</h2>
+      </div>
+      <div>
+        <Input
           value={inputValue}
           onChange={handleChange}
-          type="text"
-          placeholder="Entrez votre prénom"
+          placeholder={"Entrez votre prénom"}
+          Icon={<BsPersonCircle className="icon" />}
+          className={"input-with-icon"}
           required
         />
+
+        <button className="button-with-icon">
+          <span>Accédez à mon espace</span>
+          <IoChevronForward className="icon" />
+        </button>
       </div>
-      <button className="button-with-icon">
-        <span>Accédez à mon espace</span>
-        <IoChevronForward className="icon" />
-      </button>
 
       <Link to="/order"></Link>
     </LoginFormStyled>
@@ -47,7 +51,6 @@ export default function LoginForm() {
 }
 
 const LoginFormStyled = styled.form`
-  background: green;
   text-align: center;
   max-width: 500px;
   min-width: 400px;
@@ -70,31 +73,6 @@ const LoginFormStyled = styled.form`
     color: white;
     margin: 20px 10px 10px;
     font-size: 36px;
-  }
-
-  .input-with-icon {
-    background-color: #fff;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    padding: 18px 24px;
-    margin: 18px 0;
-
-    .icon {
-      font-size: 15px;
-      margin-right: 8px;
-      color: #93a2b1;
-    }
-
-    input {
-      border: none;
-      font-size: 15px;
-      color: #17161a;
-    }
-    &::placeholder {
-      background: white;
-      color: lightgrey;
-    }
   }
 
   .button-with-icon {
