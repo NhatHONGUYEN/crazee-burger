fcd48f63ab21bb1c7be8c69a26c18d070a27c089
NhatHONGUYEN <hnnhat67@gmail.com>
2024-03-16 17:01:53 +0100

    🍱 F05 : Add assets

--- src/component/pages/order/Main/Menu.jsx
+++ src/component/pages/order/Main/Menu.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function Menu() {
+  const [Menu, setMenu] = useState(fakeMenu);
+
   return <MenuStyled className="menu">Menu</MenuStyled>;
 }
 
