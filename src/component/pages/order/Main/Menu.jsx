01d0b089731bdd32b2a70a427f4e4f179c8c5422
NhatHONGUYEN <hnnhat67@gmail.com>
2024-03-16 17:53:51 +0100

    🚧 F05 : ORDER > MENU > Add product info

--- src/component/pages/order/Main/Menu.jsx
+++ src/component/pages/order/Main/Menu.jsx
@@ -14,7 +14,13 @@ export default function Menu() {
             <div className="image">
               <img src={produit.imageSource} alt={produit.title} />
             </div>
-            <div className="title"> {produit.title}</div>
+            <div className="info-text">
+              <div className="title"> {produit.title}</div>
+              <div className="description">
+                <div className="price">{produit.price}</div>
+                <button className="add-button">Ajouter</button>
+              </div>
+            </div>
           </div>
         );
       })}
@@ -35,5 +41,16 @@ const MenuStyled = styled.div`
     background: red;
     width: 240px;
     height: 330px;
+
+    .image {
+      border: 1px solid fuchsia;
+      width: 100px;
+      height: auto;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
   }
 `;
