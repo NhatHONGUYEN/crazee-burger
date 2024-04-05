cb926e0c1c7653c2a174f282f22ee7233214c783
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-05 17:52:30 +0200

    ♻️  F10: BAsket > Add useSuccesMessage

--- src/component/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/component/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -3,10 +3,11 @@ import { useContext, useState } from "react";
 import { EMPTY_PRODUCT } from "../../../../../../enums/product";
 import Form from "./Form.jsx";
 import SubmitButton from "./SubmitButton.jsx";
+import { useSuccesMessage } from "../../../../../../hooks/useDisplaySuccesMessage.jsx";
 
 export default function AddForm() {
   const { handleAdd, newProduct, setNewProduct } = useContext(OrderContext);
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const { isSubmitted, displaySuccesMessage } = useSuccesMessage();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -22,13 +23,6 @@ export default function AddForm() {
   const handleChange = (event) => {
     const { name, value } = event.target;
     setNewProduct({ ...newProduct, [name]: value });
-  };
-
-  const displaySuccesMessage = () => {
-    setIsSubmitted(true);
-    setTimeout(() => {
-      setIsSubmitted(false);
-    }, 2000);
   };
 
   return (
