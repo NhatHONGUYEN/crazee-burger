9199f1f77f8669780d00041c39a93d22555959ab
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-06 09:33:07 +0200

    ♻️ F11 : BASKET > Rename BasketBody into EmptyBasket

--- src/component/pages/order/Main/Basket/EmptyBasket.jsx
+++ src/component/pages/order/Main/Basket/EmptyBasket.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import { theme } from "../../../theme";
 
-export default function BasketBody() {
+export default function EmptyBasket() {
   return (
-    <BasketBodyStyled>
+    <EmptyBasketStyled>
       <span className="empty-message">Votre Commande est vide.</span>
-    </BasketBodyStyled>
+    </EmptyBasketStyled>
   );
 }
-const BasketBodyStyled = styled.div`
+const EmptyBasketStyled = styled.div`
   flex: 1;
   background: ${theme.colors.background_white};
   box-shadow: ${theme.shadows.basket};
