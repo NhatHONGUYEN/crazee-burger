7545c03ef6071e9e53a3a2a98cb067ccae7c59a2
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-11 13:40:49 +0200

    🚧   F13: BASKET > Firestore > Add "Create" from Crud

--- src/hooks/useBasket.jsx
+++ src/hooks/useBasket.jsx
@@ -6,35 +6,47 @@ import {
   findObjectById,
   findIndexById,
 } from "../utils/array";
+import { setLocalStorage } from "../utils/window";
 
 export const useBasket = () => {
   const [basket, setBasket] = useState(fakeBasket.EMPTY);
 
-  const handleAddToBasket = (idProductToAdd) => {
+  const handleAddToBasket = (idProductToAdd, username) => {
     const basketCopy = deepClone(basket);
     const productAlreadyInBasket = findObjectById(idProductToAdd, basketCopy);
 
     if (productAlreadyInBasket) {
-      incrementProductAlreadyInBasket(idProductToAdd, basketCopy);
+      incrementProductAlreadyInBasket(idProductToAdd, basketCopy), username;
       return;
     }
 
-    createNewBasketProduct(idProductToAdd, basketCopy, setBasket);
+    createNewBasketProduct(idProductToAdd, basketCopy, setBasket, username);
   };
 
-  const incrementProductAlreadyInBasket = (idProductToAdd, basketCopy) => {
+  const incrementProductAlreadyInBasket = (
+    idProductToAdd,
+    basketCopy,
+    username
+  ) => {
     const indexOfBasketProductToIncrement = findIndexById(
       idProductToAdd,
       basketCopy
     );
     basketCopy[indexOfBasketProductToIncrement].quantity += 1;
     setBasket(basketCopy);
+    setLocalStorage(username, basketCopy);
   };
 
-  const createNewBasketProduct = (idProductToAdd, basketCopy, setBasket) => {
+  const createNewBasketProduct = (
+    idProductToAdd,
+    basketCopy,
+    setBasket,
+    username
+  ) => {
     const newBasketProduct = { id: idProductToAdd, quantity: 1 };
     const newBasket = [newBasketProduct, ...basketCopy];
     setBasket(newBasket);
+    setLocalStorage(username, newBasket);
   };
 
   const handleDeleteBasketProduct = (idBasketProduct) => {
