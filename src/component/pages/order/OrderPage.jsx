534058067fd73ce4eede258ef422d3f657de73d9
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-11 14:23:48 +0200

    🐛     F13: BASKET > Firestore > Add Loader while fixing bug

--- src/component/pages/order/OrderPage.jsx
+++ src/component/pages/order/OrderPage.jsx
@@ -44,7 +44,7 @@ export default function OrderPage() {
 
   const initialiseBasket = () => {
     const basketReceived = getLocalStorage(username);
-    setBasket(basketReceived);
+    if (basketReceived) setBasket(basketReceived);
   };
 
   useEffect(() => {
