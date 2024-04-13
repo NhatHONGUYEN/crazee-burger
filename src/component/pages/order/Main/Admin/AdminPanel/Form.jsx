38313c14b45c62ca581127fcb81f64a9dc0d5a8d
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-13 15:36:02 +0200

    🚧  F15 : FORM > Add selects (ugly)

--- src/component/pages/order/Main/Admin/AdminPanel/Form.jsx
+++ src/component/pages/order/Main/Admin/AdminPanel/Form.jsx
@@ -23,6 +23,14 @@ const Form = React.forwardRef(
               ref={ref && input.name === "title" ? ref : null}
             />
           ))}
+          <select name="isAvailable" className="is-available" id="3">
+            <option value={true}>En stock</option>
+            <option value={false}>En rupture</option>
+          </select>
+          <select name="isPublicised" className="is-publicised" id="3">
+            <option value={true}>Sans pub</option>
+            <option value={false}>Avec pub</option>
+          </select>
         </div>
         <div className="form-footer">{children}</div>
       </FormStyled>
@@ -48,6 +56,7 @@ const FormStyled = styled.form`
     grid-template-rows: repeat(3, 1fr);
     grid-template-columns: repeat(3, 1fr);
     grid-row-gap: 8px;
+    grid-column-gap: 8px;
 
     .title {
       grid-area: 1/1/2/4;
@@ -57,6 +66,10 @@ const FormStyled = styled.form`
     }
     .price {
       grid-area: 3/1/4/2;
+    }
+    .is-available {
+    }
+    .is-publicised {
     }
   }
 
